2e116a1988b3beb62fd51217ca3758e1fbc83f19
Ribas93 <ribasoficial01@gmail.com>
2022-07-22 12:28:05 -0300

    using env.file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
@@ -12,10 +13,24 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"))
 */
 
+
+
+
+
+
 app.get("/json", function(req,res){
-    res.json({"message": "Hello json"})
+
+    if(process.env.MESSAGE_STYLE =='uppercase'){
+        res.json({"message":'HELLO JSON'})
+    }
+    
+    else{
+        res.json({"message":'Hello json'})
+    }
     
 })
+
+
 
 
 
